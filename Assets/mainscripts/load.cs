8c47aa363ba98d665531fc87e6cf223f79367845
Hazomg <badickii.maksim@gmail.com>
2019-12-17 06:53:46 +0700

    add load desplay and fix atack sys

---
+++ Assets/mainscripts/load.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class load : MonoBehaviour
+{
+    public int sceneID;
+    public Image loadIMG;
+    void Start()
+    {
+        StartCoroutine(AsyncLoad());
+    }
+
+    IEnumerator AsyncLoad() {
+
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneID);
+        while (!loading.isDone)
+        {
+            float prog = loading.progress / 0.9f;
+            loadIMG.fillAmount = loading.progress;
+            yield return null;
+
+        }
+    }
+}
