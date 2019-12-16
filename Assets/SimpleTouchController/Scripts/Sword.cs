53b6555b0f57f127b18c2d52b2819e4cb13bec5f
bestofoleg <olegabramov1998@gmail.com>
2019-12-16 20:07:59 +0400

    Addition files for old commit

---
+++ Assets/SimpleTouchController/Scripts/Sword.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sword : MonoBehaviour
+{
+    private const string EnemyTeg = "Enemy";
+    
+    public int damage;
+
+    [Range(0, 1)]
+    private float scale;
+
+    public int RealDamage
+    {
+        get => realDamage;
+    }
+
+    public Collider swordCollider;
+
+    private int realDamage;
+
+    private void Awake()
+    {
+        swordCollider = GetComponent<Collider>();
+        scale = 1;
+        RecalculateRealDamage();
+    }
+
+    private void RecalculateRealDamage() => realDamage = (int) (damage * scale);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals(EnemyTeg))
+        {
+            Dummy dummy = other.GetComponent<Dummy>();
+            RecalculateRealDamage();
+            dummy.Damage(realDamage);
+        }
+    }
+
+    public void hightAttack()
+    {
+        scale = 1.5f;
+        RecalculateRealDamage();
+    }
+
+    public void mediumAttack()
+    {
+        scale = 1f;
+        RecalculateRealDamage();
+    }
+}
---
+++ Assets/Utils/Dummy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Dummy : MonoBehaviour
+{
+    private const string DummyIsDeathMessage = "Dummy is death!";
+    
+    private int health;
+
+    public BulkMessageWordUI damageQuantityTextUI;
+    
+    public int maxHealth;
+
+    private void Awake()
+    {
+        health = maxHealth;
+    }
+
+    public void Damage(int damage)
+    {
+        BulkMessageWordUI damageQuantity = Instantiate(damageQuantityTextUI);
+        damageQuantity.StartBulk(this.transform);
+        if (health > 0)
+        {
+            health -= damage;
+            damageQuantity.text.text = damage.ToString();
+        }
+        else
+        {
+            damageQuantity.text.text = DummyIsDeathMessage;
+        }
+    }
+}
