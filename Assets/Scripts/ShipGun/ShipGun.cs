199c2ba56555b5957463b7fcb03a68a7d9a1a21f
bestofoleg <olegabramov1998@gmail.com>
2020-02-17 19:09:50 +0400

    fixed ship controll system when we have a spawn of player

---
+++ Assets/Scripts/ShipGun/ShipGun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipGun : MonoBehaviour {
+    public Rigidbody bulletPrefab;
+
+    public Transform bulletSpawnPoint;
+
+    public float gunForce;
+
+    public void shoot() {
+        Rigidbody currentBullet = GameObject.Instantiate(
+            bulletPrefab,
+            bulletSpawnPoint.transform.position,
+            bulletSpawnPoint.transform.rotation
+        );
+        currentBullet.AddForce(bulletSpawnPoint.transform.forward * gunForce);
+    }
+}
