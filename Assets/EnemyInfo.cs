807e8f8cd4d9bc4505a02345e290c5dfa3648dd0
Hazomg <badickii.maksim@gmail.com>
2020-02-26 23:25:17 +0700

    dungeon 98% complete and add new ui

---
+++ Assets/EnemyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour
+{
+    public List<GameObject> enemy;
+    public int enemy_info;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        enemy_info = enemy.Count;
+
+    }
+}
---
+++ Assets/OffonCD.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffonCD : MonoBehaviour
+{
+    GameObject[] offmenu;
+    GameObject[] onmenu;
+    void Start()
+    {
+        offmenu = GameObject.FindGameObjectsWithTag("offmenu");
+        onmenu = GameObject.FindGameObjectsWithTag("onmenu");
+    }
+
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < onmenu.Length; i++)
+        {
+
+            onmenu[i].SetActive(false);
+        }
+
+        for (int i = 0; i < offmenu.Length; i++)
+        {
+
+            offmenu[i].SetActive(true);
+        }
+
+    }
+}
